7d7c1cf48cf64a13f625d78d91e9a54f28f7df79
Olena Lisova <ymenyaoleni@gmail.com>
2026-06-05 16:39:10 +0300

    Revert "Add comment about average sales"

--- analysis.py
+++ analysis.py
@@ -5,7 +5,5 @@ df = pd.DataFrame(data)
 
 print("Продажі по містах:")
 print(df)
-average_sales = df["sales"].mean()
-print("Середнє значення:", average_sales)
-print("Це середній рівень продажів по трьох містах")
+print("Середнє значення:", df["sales"].mean())
 print("Максимальні продажі:", df["sales"].max())
